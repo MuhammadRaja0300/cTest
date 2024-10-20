fb8d34aff88660d0165df48bae00b6cc0113322c
muhammad <muhammadraja0300@gmail.com>
2024-10-20 06:06:41 +0500

    commit

--- src/main/java/runners/RunAllTestCases.java
+++ src/main/java/runners/RunAllTestCases.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/java/features/*.feature", glue = {"src/test/java/stepDefinitions"},
         //tags= "@regression" ,c
 
-        plugin = {"pretty", "json:target/cucumber.json", "html:target/CucumberReport.html"}
+        plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber.html"}
 
 
 )
