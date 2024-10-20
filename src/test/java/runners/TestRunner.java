b4fc770437877d76fd877df6cfe68d7359c0e629
muhammad <muhammadraja0300@gmail.com>
2024-10-20 08:25:51 +0500

    update tRunner g

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = "stepDefinitions",
-        //tags= "@regression" ,c
+        //tags= "@regression" ,
         publish = true,
         plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber.html"}
 
